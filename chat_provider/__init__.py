774b27ca93f197f0b9d5cdfd5940e3fa6897ad48
Peppy Sisay <peppy@dave.com>
2019-12-12 22:48:06 -0800

    Uncomment

--- chat_provider/__init__.py
+++ chat_provider/__init__.py
@@ -1,2 +1,2 @@
-# from chat_provider.chat_provider import ChatProvider, ChatProviderSource
-# from chat_provider.group_me import GroupMeClient, GroupMeWebhookMessage
+from chat_provider.chat_provider import ChatProvider, ChatProviderSource
+from chat_provider.group_me import GroupMeClient, GroupMeWebhookMessage
